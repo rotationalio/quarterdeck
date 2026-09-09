c0ca45cce3df9f33baae217e83e7d541cae7890e
Benjamin Bengfort <benjamin@bengfort.com>
2026-09-09 10:09:45 -0500

    version bump v0.15.0

--- pkg/version.go
+++ pkg/version.go
@@ -11,10 +11,10 @@ import (
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 14
+	VersionMinor         = 15
 	VersionPatch         = 0
 	VersionReleaseLevel  = "alpha"
-	VersionReleaseNumber = 20
+	VersionReleaseNumber = 21
 )
 
 // Set the GitVersion via -ldflags="-X 'go.rtnl.ai/quarterdeck/pkg.GitVersion=$(git rev-parse --short HEAD)'"
