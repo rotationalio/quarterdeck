702191b04ba77ced26bb8a35983f9aea8dcaeb3d
Chris Okuda <chris@okuda.dev>
2026-09-23 12:22:02 -1000

    Bump version to v0.17.0-alpha-23

--- pkg/version.go
+++ pkg/version.go
@@ -11,10 +11,10 @@ import (
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 16
+	VersionMinor         = 17
 	VersionPatch         = 0
 	VersionReleaseLevel  = "alpha"
-	VersionReleaseNumber = 22
+	VersionReleaseNumber = 23
 )
 
 // Set the GitVersion via -ldflags="-X 'go.rtnl.ai/quarterdeck/pkg.GitVersion=$(git rev-parse --short HEAD)'"
